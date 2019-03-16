f6ed96fbc033a50e79ca59ddb8bb534c9fecfb53
lydsnm <dongingdao@163.com>
2019-03-16 22:15:00 +0800

    Initial Commit

---
+++ IdeaCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdeaCheck
+//
+//  Created by Apple on 2019/3/16.
+//  Copyright © 2019 lydsnm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
