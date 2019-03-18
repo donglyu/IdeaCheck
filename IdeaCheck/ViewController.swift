6a8a5bedf63aee2dbea9370a722967a18ffb147f
lydsnm <dongingdao@163.com>
2019-03-18 09:30:40 +0800

    first upload

--- IdeaCheck/ViewController.swift
+++ IdeaCheck/ViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    lazy var tableView:UITableView = {
+        
+        let _tableview = UITableView.init(frame: UIScreen.main.bounds)
+        _tableview.delegate = self
+        _tableview.dataSource = self
+        _tableview.register(UITableViewCell.self, forCellReuseIdentifier: "idea")
+        self.view.addSubview(_tableview)
+        return _tableview
+        
+    }()
+    
+    var Ideas:Array<CellNameModel>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Ideas = [CellNameModel(name: "name", jumpClass: WordShowCtrler.self)]
+        
+        
+        
     }
 
 
+}
+
+
+extension ViewController{
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let ideaModel = Ideas[indexPath.row]
+//        let vc = (ideaModel.jumpClass as UIViewController)
+        let nac = UINavigationController.init(rootViewController: <#T##UIViewController#>)
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idea", for: indexPath)
+        
+        
+        
+        
+        return cell
+    }
+    
+    
+}
+
+
+
+struct CellNameModel {
+    
+    var name:NSString?
+    
+    var jumpClass:AnyClass
+    
+    
+    
 }
 
